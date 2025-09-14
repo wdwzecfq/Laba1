8f81e1af92f5979935a035b663f3d1a5aea487f1
User <User@DESKTOP-GUSPOQO>
2025-09-14 17:00:27 +0300

    Добавьте файлы проекта.

---
+++ Laba1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Длина окружности = ");
+
+double C = Convert.ToDouble(Console.ReadLine());
+double PI = Math.PI;
+double r = C / (2 * PI);
+double S = PI * Math.Pow(r, 2);
+
+Console.WriteLine($"Площадь круга = {S}.");
